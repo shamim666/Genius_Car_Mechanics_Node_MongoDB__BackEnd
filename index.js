cd5a46adef7afc7bd296532ea43fb5bf783bb83f
shamim <shamim.pstu@gmail.com>
2022-03-15 15:04:09 +0600

    port modified

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 //middleware
